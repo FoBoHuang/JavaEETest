348b1a779dfa219e260e9019ab17b4b6bf35a7e1
marvHuang <hfb161716@163.com>
2018-09-14 00:18:05 +0800

    [add&revice-26]删除sql文件

--- Test26-Security/src/main/java/org/sang/Service/CustomUserService.java
+++ Test26-Security/src/main/java/org/sang/Service/CustomUserService.java
@@ -14,12 +14,12 @@ public class CustomUserService implements UserDetailsService {
     @Autowired
     SysUserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        SysUser user = userRepository.findByUsername(s);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        SysUser user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("用户名不存在");
         }
-        System.out.println("s:"+s);
+        System.out.println("username:" + username);
         System.out.println("username:"+user.getUsername()+";password:"+user.getPassword());
         return user;
     }
