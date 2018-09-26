6be7a1eff197048822c1d2e9a6ad5ffe5e6d4e64
marvHuang <hfb161716@163.com>
2018-09-26 21:05:45 +0800

    [add&revice]添加有关点对点通信的一些方法和类的理解

--- Test21-WebChat/src/main/java/org/sang/Controller/WsController.java
+++ Test21-WebChat/src/main/java/org/sang/Controller/WsController.java
@@ -12,11 +12,16 @@ import java.security.Principal;
  */
 @Controller
 public class WsController {
+
+    /*SimpMessagingTemplate实现向浏览器发送消息的功能*/
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+    /*在Spring MVC中，可以直接在参数中获取Principal，Principal中包含有当前用户的用户名*/
     @MessageMapping("/chat")
     public void handleChat(Principal principal, String msg) {
+
+        /*convertAndSendToUser方法是向用户发送一条消息，第一个参数是目标用户用户名，第二个参数是浏览器中订阅消息的地址，第三个参数是消息本身*/
         if (principal.getName().equals("sang")) {
             messagingTemplate.convertAndSendToUser("lenve", "/queue/notifications", principal.getName() + "给您发来了消息：" + msg);
         }else{
